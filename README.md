314346c6b7cfc571b6a22e54ffd5c4ca13b4e58e
bsteen <steenkamerben@gmail.com>
2015-11-05 18:10:26 -0500

    Started drawing commands.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # OpenGL_Test
 A test program I am using to learn OpenGL.
+The ultimate goal of this project is to display two pictures side by side.
 
-Uses OpenGL, SFML, and GLEW 
+It uses OpenGL, SFML, and GLEW
+
+I am using this tutorial to make the program:
+https://open.gl/
--- test.cpp
+++ test.cpp
@@ -3,27 +3,44 @@
 #include <SFML/Window.hpp>
 #include <iostream>
 
+//The code in this program comes from https://open.gl
+
 int main(){
-    //Initiliaze window setting values
+    //Initiliaze window setting values--------------
     sf::ContextSettings settings;
     settings.depthBits = 24;
     settings.stencilBits = 8;
     //settings.antialiasingLevel = 2; // Optional
 
-    //Create window
+    //Create window---------------------------------
     sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Close, settings);
 
-
-    glewExperimental = GL_TRUE;
+    glewExperimental = GL_TRUE;//Force GLEW to use a modern OpenGL method for checking if a function is available.
     glewInit();
 
-    GLuint vertexBuffer;
-    glGenBuffers(1, &vertexBuffer);
 
-    //Is the vertexBuffer succesfully created?
-    std::cout << vertexBuffer << std::endl;
+    //Prepare to draw shape-------------------------
+    float vertices[] = {
+        0.0f,  0.5f, // Vertex 1 (X, Y)
+        0.5f, -0.5f, // Vertex 2 (X, Y)
+        -0.5f, -0.5f  // Vertex 3 (X, Y)
+    };//A triangle.
 
-    //Run main window loop
+    //Make buffer (Vertex Buffer Object, "vbo") to store data
+    GLuint vbo; //GL unsigned int to store pointer of object.
+    glGenBuffers(1, &vbo); //Generate 1 buffer
+
+    //Make Vertex Buffer Object the active array buffer.
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    //Copy vertices to the active buffer.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //GL_STATIC_DRAW => The vertex data will be uploaded once and drawn many times
+    //Determines in what kind of memory the data is stored on the graphics card for the highest efficiency.
+
+    //Add vertex shaders
+
+    //Run main window loop--------------------------
     bool running = true;
     while (running){
         sf::Event windowEvent;
@@ -31,7 +48,7 @@ int main(){
         while (window.pollEvent(windowEvent)){
 
             switch (windowEvent.type){
-                
+
                 case sf::Event::Closed:
                     running = false;
                     break;
